11e5e28c8edda2947498061a080c438a3186cf65
Vladix97 <gramovv@gmail.com>
2017-02-20 13:16:12 +0200

    added p03 100 / 100

--- TheExam/src/p03.java
+++ TheExam/src/p03.java
@@ -1,6 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -10,68 +10,51 @@ public class p03 {
     public static void main(String[] args) throws Exception {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
-        Map<String, String> map = new HashMap<>();
+        Map<String, String> map = new LinkedHashMap<>();
 
         Pattern pattern = Pattern.compile("(\\,|\\_)([a-zA-Z0-9]+)(?<![0-9])([0-9])");
 
         String command = in.readLine().trim();
         while (!"Ascend".equals(command)) {
-            Matcher matcher = pattern.matcher(command);
 
-            while (matcher.find()) {
-                boolean shouldAdd = true;
-
-                String commaUnderscore = matcher.group(1);
-                if (!commaUnderscore.equals(",") && !commaUnderscore.equals("_"))
-                    throw new Exception();
-
-                char[] message = matcher.group(2).toCharArray();
-                int digit = Integer.parseInt(matcher.group(3));
-
-                String search = commaUnderscore + new String(message) + digit;
-                if (map.containsKey(search)) {
-                    String newPattern = map.get(search);
-                    command = command.replace(search, newPattern);
-
-                    map.put(search, newPattern);
-
-                    matcher = pattern.matcher(command);
-                    continue;
-                }
-
-
-                String messageAsString = commaUnderscore + new String(message) + digit;
-                if (map.containsKey(messageAsString)) {
-                    message = map.get(messageAsString).toCharArray();
-                    shouldAdd = false;
-                }
-
-                if (commaUnderscore.equals(",")) {
-                    processMessage(message, digit);
-                } else {
-                    processMessage(message, -digit);
-                }
-
-                String toReplace = matcher.group();
-                if (shouldAdd) {
-                    map.put(toReplace, new String(message));
-                }
-
-                command = command.replace(matcher.group(), new String(message));
-
-                matcher = pattern.matcher(command);
+            for (String str : map.keySet()) {
+                command = command.replaceAll(str, map.get(str));
             }
 
+            Matcher matcher = pattern.matcher(command);
+            while (matcher.find()) {
+                String commaUnderscore = matcher.group(1);
+                String message = matcher.group(2);
+                int digit = Integer.parseInt(matcher.group(3));
+
+                String wholeMatch = matcher.group(0);
+
+                String newWord = "";
+                if (commaUnderscore.equals(",")) {
+                    newWord = processMessage(message, digit);
+                } else {
+                    newWord = processMessage(message, -digit);
+                }
+
+                command = command.replaceAll(wholeMatch, newWord);
+
+                map.put(wholeMatch, newWord);
+            }
 
             System.out.println(command);
 
             command = in.readLine();
+
         }
     }
 
-    private static void processMessage(char[] message, int digit) {
-        for (int i = 0; i < message.length; i++) {
-            message[i] += digit;
+    private static String processMessage(String message, int digit) {
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = 0; i < message.length(); i++) {
+            builder.append(Character.toString((char)(message.charAt(i) + digit)));
         }
+
+        return builder.toString();
     }
 }
